65c27dd5e3545074029aa8c66f132cbd7b794655
kairat <kairatkubanychbekov96@gmail.com>
2022-10-17 12:15:00 +0300

    home-work-6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  let [count, setCount] = React.useState(+localStorage.getItem("number"));
+  let local = localStorage.setItem("number", count);
+  let onClickPlus = () => {
+    setCount(count + 1);
+  };
+  let onClickMinus = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>счетчик</h2>
+        <h1>{count}</h1>
+        <div className="button">
+          <button className="add" onClick={onClickMinus} disabled={count === 0}>
+            {" "}
+            --мминус
+          </button>
+          <button className="add" onClick={onClickPlus}>
+            {" "}
+            +плюс
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
